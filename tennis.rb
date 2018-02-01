5835a511716f8a20f0f948aec577dfc944f32d04
James <jaimersonaraujo@gmail.com>
2018-02-01 08:49:21 -0300

    Refactored TennisGame1

--- tennis.rb
+++ tennis.rb
@@ -1,58 +1,70 @@
 
 class TennisGame1
+  Player = Struct.new(:name, :points) do
+    def add_point
+      self.points += 1
+    end
+
+    def <=>(other)
+      self.points <=> other.points
+    end
+  end
+
+  SCORE_NAMES = {
+    0 => "Love",
+    1 => "Fifteen",
+    2 => "Thirty",
+    3 => "Forty"
+  }.freeze
+
+  attr_reader :players
 
   def initialize(player1Name, player2Name)
-    @player1Name = player1Name
-    @player2Name = player2Name
-    @p1points = 0
-    @p2points = 0
+    @players = [Player.new(player1Name, 0), Player.new(player2Name, 0)]
   end
-        
-  def won_point(playerName)
-    if playerName == @player1Name
-      @p1points += 1
-    else
-      @p2points += 1
-    end
+
+  def won_point(player_name)
+    players.find { |p| p.name == player_name }.add_point
   end
-  
+
   def score
-    result = ""
-    tempScore=0
-    if (@p1points==@p2points)
-      result = {
-          0 => "Love-All",
-          1 => "Fifteen-All",
-          2 => "Thirty-All",
-      }.fetch(@p1points, "Deuce")
-    elsif (@p1points>=4 or @p2points>=4)
-      minusResult = @p1points-@p2points
-      if (minusResult==1)
-        result ="Advantage " + @player1Name
-      elsif (minusResult ==-1)
-        result ="Advantage " + @player2Name
-      elsif (minusResult>=2)
-        result = "Win for " + @player1Name
-      else
-        result ="Win for " + @player2Name
-      end
+    return same_score_result if same_score?
+    return result_for_score_over_4_points if score_over_4?
+    result_for_score_below_4_points
+  end
+
+  private
+
+  def same_score?
+    players.max == players.min
+  end
+
+  def score_over_4?
+    players.any? { |p| p.points >= 4 }
+  end
+
+  def result_for_score_below_4_points
+    players.map do |player|
+      SCORE_NAMES.fetch(player.points)
+    end.join('-')
+  end
+
+  def result_for_score_over_4_points
+    loser, winner = players.sort
+
+    if(winner.points - loser.points >= 2)
+      return "Win for #{winner.name}"
     else
-      (1...3).each do |i|
-        if (i==1)
-          tempScore = @p1points
-        else
-          result+="-"
-          tempScore = @p2points
-        end
-        result += {
-            0 => "Love",
-            1 => "Fifteen",
-            2 => "Thirty",
-            3 => "Forty",
-        }[tempScore]
-      end
+      return "Advantage #{winner.name}"
     end
-    result
+  end
+
+  def same_score_result
+    {
+      0 => "Love-All",
+      1 => "Fifteen-All",
+      2 => "Thirty-All",
+    }.fetch(players.first.points, "Deuce")
   end
 end
 
@@ -63,7 +75,7 @@ class TennisGame2
     @p1points = 0
     @p2points = 0
   end
-      
+
   def won_point(playerName)
     if playerName == @player1Name
       p1Score()
@@ -89,7 +101,7 @@ class TennisGame2
     if (@p1points==@p2points and @p1points>2)
         result = "Deuce"
     end
-    
+
     p1res = ""
     p2res = ""
     if (@p1points > 0 and @p2points==0)
@@ -115,11 +127,11 @@ class TennisGame2
       if (@p2points==3)
         p2res = "Forty"
       end
-      
+
       p1res = "Love"
       result = p1res + "-" + p2res
     end
-    
+
     if (@p1points>@p2points and @p1points < 4)
       if (@p1points==2)
         p1res="Thirty"
@@ -180,7 +192,7 @@ class TennisGame2
   def p1Score
     @p1points +=1
   end
-  
+
   def p2Score
     @p2points +=1
   end
@@ -193,7 +205,7 @@ class TennisGame3
     @p1 = 0
     @p2 = 0
   end
-      
+
   def won_point(n)
     if n == @p1N
         @p1 += 1
@@ -201,7 +213,7 @@ class TennisGame3
         @p2 += 1
     end
   end
-  
+
   def score
     if (@p1 < 4 and @p2 < 4) and (@p1 + @p2 < 6)
       p = ["Love", "Fifteen", "Thirty", "Forty"]
